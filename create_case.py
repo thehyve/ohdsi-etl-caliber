490c2ef2089be7a570c1c23889e8f2cc4f498f29
Spayralbe <stefan@thehyve.nl>
2017-10-27 18:19:51 +0200

    Add CASE creation script

---
+++ create_case.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import csv
+import sys
+from os import linesep
+
+import click
+
+
+def write_output(result, output_file):
+    if output_file:
+        with open(output_file, 'w') as out:
+            for line in result:
+                out.write(line + linesep)
+    else:
+        print('\n'.join(result))
+
+
+def check_type(value):
+    try:
+        int(value)
+    except ValueError:
+        value = '\'' + value + '\''
+    return value
+
+
+def process_file(file_path, source, output_file):
+    result = ['CASE ' + source]
+
+    with open(file_path, 'r') as lookup:
+        reader = csv.reader(lookup, delimiter='\t')
+        print('Header:', '\t'.join(next(reader)) + linesep)  # Show header
+        for row in reader:
+            if len(row) == 0:  # Skip blank lines
+                continue
+            elif len(row) != 2:
+                print('Unexpected number of values: {0}'.format(row))
+                print('\nRow will be ignored')
+            source_value = check_type(row[0])
+            new_value = check_type(row[1])
+            new_line = 'WHEN ' + source_value + ' THEN ' + new_value
+            result.append(new_line)
+    result.append('END')
+
+    write_output(result, output_file)
+
+
+@click.command()
+@click.argument('file_path', nargs=1, type=click.Path(exists=True, dir_okay=False), metavar='<path>')
+@click.option('--source', '-s', default='<source>', metavar='<str>',
+              help='The (alias) name of the source column that requires translation')
+@click.option('--output_file', '-o', default=None, metavar='<path>',
+              help='Write output to file instead of print to console')
+def main(file_path, source, output_file):
+    """This script creates formatted sql code for CASE translations."""
+    print(linesep + 'file_path:', file_path)
+    print('Source column:', source)
+
+    process_file(file_path, source, output_file)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
