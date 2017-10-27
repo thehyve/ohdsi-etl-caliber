79d6328900197e5aeaa23faa26b6daa76649f614
Spayralbe <stefan@thehyve.nl>
2017-10-27 18:09:42 +0200

    Update provider formatting

--- sql/loading/provider.sql
+++ sql/loading/provider.sql
@@ -6,45 +6,45 @@ INSERT INTO cdm5.provider (
   specialty_source_value
 )
 
-SELECT
-  DISTINCT ON (staff.staffid)
-  staff.staffid                                       AS provider_id,
+  SELECT
+    DISTINCT ON (staff.staffid)
+    staff.staffid                                       AS provider_id,
 
-  CASE staff.gender
-  WHEN 1
-    THEN 8507 -- MALE
-  WHEN 2
-    THEN 8532 -- FEMALE
-  ELSE 0 -- Data not entered | Indeterminate | Unknown
-  END                                                 AS gender_concept_id,
+    CASE staff.gender
+    WHEN 1
+      THEN 8507 -- MALE
+    WHEN 2
+      THEN 8532 -- FEMALE
+    ELSE 0 -- Data not entered | Indeterminate | Unknown
+    END                                                 AS gender_concept_id,
 
-  staff.gender                                        AS gender_source_value,
+    staff.gender                                        AS gender_source_value,
 
-  coalesce(specialty_map.target_concept_id, 38004514) AS specialty_concept_id,
+    coalesce(specialty_map.target_concept_id, 38004514) AS specialty_concept_id,
 
-  staff.role :: VARCHAR                               AS specialty_source_value
+    staff.role :: VARCHAR                               AS specialty_source_value
 
-FROM caliber.staff AS staff
-  LEFT JOIN cdm5.source_to_concept_map AS specialty_map
-    ON staff.role = CAST(specialty_map.source_code AS INT)
-       AND specialty_map.source_vocabulary_id = 'JNJ_CPRD_PROV_SPEC'
+  FROM caliber.staff AS staff
+    LEFT JOIN cdm5.source_to_concept_map AS specialty_map
+      ON staff.role = (specialty_map.source_code :: INT)
+         AND specialty_map.source_vocabulary_id = 'JNJ_CPRD_PROV_SPEC'
 
-UNION ALL
+  UNION ALL
 
-SELECT DISTINCT ON (hoc.tretspef)
+  SELECT DISTINCT ON (hoc.tretspef)
 
-  CASE tretspef
-  WHEN '&amp;'
-    THEN 2000000000
-  ELSE 2000000000 + (tretspef :: INT)
-  END
-                                        AS provider_id,
+    CASE tretspef
+    WHEN '&amp;'
+      THEN 2000000000
+    ELSE 2000000000 + (tretspef :: INT)
+    END
+                                          AS provider_id,
 
-  0                                     AS gender_concept_id,
-  NULL                                  AS gender_source_value,
-  coalesce(target_concept_id, 38004514) AS specialty_concept_id,
-  tretspef                              AS specialty_source_value
+    0                                     AS gender_concept_id,
+    NULL                                  AS gender_source_value,
+    coalesce(target_concept_id, 38004514) AS specialty_concept_id,
+    tretspef                              AS specialty_source_value
 
-FROM caliber.hes_op_clinical AS hoc
-  LEFT JOIN cdm5.source_to_concept_map AS cms2_map
-    ON hoc.tretspef = cms2_map.source_code AND cms2_map.source_vocabulary_id = 'JNJ_CPRD_PROV_CMS2'
+  FROM caliber.hes_op_clinical AS hoc
+    LEFT JOIN cdm5.source_to_concept_map AS cms2_map
+      ON hoc.tretspef = cms2_map.source_code AND cms2_map.source_vocabulary_id = 'JNJ_CPRD_PROV_CMS2';
