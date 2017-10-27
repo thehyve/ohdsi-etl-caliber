52be37997741180abb7218bda70e13af4b1ad8e2
Maxim Moinat <maxim@thehyve.nl>
2017-10-27 14:22:43 +0200

    separated tests from sql functions

--- sql/functions/createEndDate.sql
+++ sql/functions/createEndDate.sql
@@ -10,24 +10,3 @@ BEGIN
   RETURN startDate + (durationDays - 1) * INTERVAL '1 day';
 END;
 $$ LANGUAGE plpgsql;
-
-/*
-Unit tests
- */
-WITH cases (expected, actual) AS (
-  VALUES
-    (to_date('20120409', 'yyyymmdd'), createEndDate(to_date('20120409', 'yyyymmdd'), 1))
-    ,(to_date('20120410', 'yyyymmdd'), createEndDate(to_date('20120409', 'yyyymmdd'), 2))
-    ,(to_date('20120418', 'yyyymmdd'), createEndDate(to_date('20120409', 'yyyymmdd'), 10))
-    ,(to_date('20160407', 'yyyymmdd'), createEndDate(to_date('20150409', 'yyyymmdd'), 365))
-)
-SELECT
-  CASE
-  WHEN expected = actual THEN 'passed'
-  WHEN expected ISNULL AND actual ISNULL THEN 'passed'
-  ELSE 'FAILED'
-  END,
-  expected,
-  actual
-FROM cases
-;
