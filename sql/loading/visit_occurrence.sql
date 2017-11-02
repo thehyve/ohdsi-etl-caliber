fe9342bf065d870890a6ee607df92dd41cd3582e
Spayralbe <stefan@thehyve.nl>
2017-11-02 16:40:40 +0100

    Add hes_diag_hosp source to visit occurence

--- sql/loading/visit_occurrence.sql
+++ sql/loading/visit_occurrence.sql
@@ -1,68 +1,69 @@
 
 INSERT INTO cdm5.visit_occurrence
 (
- visit_occurrence_id,
- person_id,
- visit_start_date,
- visit_end_date,
- visit_concept_id, -- When from CPRD.patient: ‘9202’ - Outpatient Visit When from HES_diag_hosp: ‘9201’ - Inpatient Visit
- provider_id,
- visit_type_concept_id, -- ‘44818518’ ‘Visit derived from EHR record’
- visit_source_concept_id,
- admitting_source_value,
- discharge_to_concept_id,
- discharge_to_source_value,
- preceding_visit_occurrence_id,
- visit_start_datetime,
- visit_end_datetime,
- care_site_id,
- visit_source_value
-);
+  visit_occurrence_id,
+  person_id,
+  care_site_id,
+  visit_start_date,
+  visit_end_date,
+  visit_concept_id,
+  visit_source_value,
+  provider_id,
+  visit_type_concept_id
+)
+  SELECT
+    -- Only one visit per patient and date
+    DISTINCT ON (consultation.patid, consultation.eventdate)
 
-SELECT
-  cons.patid,
-  cons.voi,
-  cons.eventdate AS start_date,
-  cons.eventdate AS end_date,
-  cons.visit_concept_id,
-  44818518       AS visit_type_concept_id,
-  -- Visit derived from EHR record
-  cons.provider_id,
-  cons.source_val
-FROM (
-       SELECT
-         patid,
-         patid * 10000000000 + (EXTRACT(YEAR FROM eventdate) * 10000) +
-         (EXTRACT(MONTH FROM eventdate) * 100) + EXTRACT(DAY FROM eventdate) AS voi,
-         eventdate,
-         9202                                                                AS visit_concept_id,
-         CASE
-         WHEN staffid = 0
-           THEN NULL
-         ELSE staffid
-         END                                                                 AS provider_id,
-         patid :: TEXT || ':' || eventdate                                   AS source_val
-       FROM caliber.consultation
-     ) cons
-UNION
-SELECT
-  hdh.patid,
-  hdh.voi,
-  hdh.admidate   AS start_date,
-  hdh.discharged AS end_date,
-  hdh.visit_concept_id,
-  44818518       AS visit_type_concept_id,
-  -- Visit derived from EHR record
-  NULL           AS provider_id,
-  hdh.source_val
-FROM (
-       SELECT
-         patid,
-         spno         AS voi,
-         admidate,
-         discharged,
-         9201         AS visit_concept_id,
-         spno :: TEXT AS source_val
-       FROM caliber.hes_diag_hosp
-     ) hdh;
+    -- Patid * 10000000000  + (year(eventdate)*10000)  + (month(eventdate)* 100)  + day(eventdate)
+    createVisitId(consultation.patid, consultation.eventdate) AS visit_occurrence_id,
 
+    consultation.patid                                        AS person_id,
+
+    patient.pracid                                            AS care_site_id,
+
+    consultation.eventdate                                    AS visit_start_date,
+
+    consultation.eventdate                                    AS visit_end_date,
+
+    -- 'Outpatient Visit'
+    9202                                                      AS visit_concept_id,
+
+    'consultation'                                            AS visit_source_value,
+
+    consultation.staffid                                      AS provider_id,
+
+    44818518                                                  AS visit_type_concept_id -- 'Visit derived from EHR record'
+
+  FROM caliber.consultation AS consultation
+    JOIN caliber.patient AS patient
+      ON consultation.patid = patient.patid
+  WHERE consultation.eventdate IS NOT NULL
+
+  UNION ALL
+
+  SELECT DISTINCT ON (hes_diag_hosp.patid)
+    hes_diag_hosp.spno                                         AS visit_occurrence_id,
+
+    hes_diag_hosp.patid                                        AS person_id,
+
+    patient.pracid                                             AS care_site_id,
+
+    hes_diag_hosp.admidate                                     AS visit_start_date,
+
+    -- Use start date if no end date available
+    coalesce(hes_diag_hosp.discharged, hes_diag_hosp.admidate) AS visit_end_date,
+
+    9201                                                       AS visit_concept_id,
+
+    -- Inpatient visit
+    'hes_diag_hosp'                                            AS visit_source_value,
+
+    NULL                                                       AS provider_id,
+
+    44818518                                                   AS visit_type_concept_id -- Visit derived from EHR record
+
+  FROM caliber.hes_diag_hosp AS hes_diag_hosp
+    JOIN caliber.patient AS patient
+      ON hes_diag_hosp.patid = patient.patid
+  WHERE hes_diag_hosp.admidate IS NOT NULL;
