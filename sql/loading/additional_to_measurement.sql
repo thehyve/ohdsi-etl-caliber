5427dbc1ac8a3d42611d66f0649c78f76b501820
Maxim Moinat <maxim@thehyve.nl>
2017-11-24 14:36:27 +0100

    fixed join of clinical on additional

--- sql/loading/additional_to_measurement.sql
+++ sql/loading/additional_to_measurement.sql
@@ -16,7 +16,7 @@ INSERT INTO cdm5.measurement
   unit_source_value
 )
   SELECT
-    clinical.patid AS person_id,
+    additional.patid AS person_id,
 
     clinical.eventdate AS measurement_date,
 
@@ -25,12 +25,12 @@ INSERT INTO cdm5.measurement
     clinical.staffid AS provider_id,
 
     CASE
-    WHEN createvisitid(additional_int.patid, clinical.eventdate) IN (SELECT visit_occurrence_id FROM cdm5.visit_occurrence)
-      THEN createvisitid(additional_int.patid, clinical.eventdate)
+    WHEN createvisitid(additional.patid, clinical.eventdate) IN (SELECT visit_occurrence_id FROM cdm5.visit_occurrence)
+      THEN createvisitid(additional.patid, clinical.eventdate)
     ELSE NULL
     END AS visit_occurrence_id,
 
-    CASE WHEN additional_int.enttype_string LIKE '372%'
+    CASE WHEN additional.enttype_string LIKE '372%'
       -- Patient Reported Value
       THEN 44818704
       -- Lab Result
@@ -39,10 +39,10 @@ INSERT INTO cdm5.measurement
 
     coalesce(enttype_map.target_concept_id, 0) AS measurement_concept_id,
 
-    additional_int.enttype_string AS measurement_source_value,
+    additional.enttype_string AS measurement_source_value,
 
     -- Numeric value
-    additional_int.data_value as value_as_number,
+    additional.data_value as value_as_number,
 
     -- Mapping of the description
     mapCprdLookup(cprd_lookup.description) AS value_as_concept_id,
@@ -50,28 +50,27 @@ INSERT INTO cdm5.measurement
     -- Description of the code belonging to that lookup type, or if no data_code present, the date or lookup_type
     coalesce(
         cprd_lookup.description,
-        additional_int.data_code :: TEXT,
-        additional_int.data_date :: TEXT,
-        additional_int.lookup_type
+        additional.data_code :: TEXT,
+        additional.data_date :: TEXT,
+        additional.lookup_type
     ) AS value_as_string,
 
     unit_map.target_concept_id AS unit_concept_id,
 
-    additional_int.unit_code AS unit_source_value
+    additional.unit_code AS unit_source_value
 
-  FROM public.additional_intermediate as additional_int
-  LEFT JOIN caliber_real.clinical AS clinical
-    ON clinical.adid =  additional_int.adid
+  FROM public.additional_intermediate as additional
+  LEFT JOIN caliber_real.clinical AS clinical USING(adid, patid)
   LEFT JOIN cdm5.source_to_concept_map AS enttype_map
-      ON enttype_map.source_code = additional_int.enttype_string AND
+      ON enttype_map.source_code = additional.enttype_string AND
          enttype_map.source_vocabulary_id IN ('JNJ_CPRD_ET_LOINC','JNJ_CPRD_SCORE_LOINC')
   LEFT JOIN cdm5.concept AS target_concept
       ON target_concept.concept_id = enttype_map.target_concept_id
   LEFT JOIN public.cprd_lookup AS cprd_lookup
-      ON cprd_lookup.lookup_type = additional_int.lookup_type AND
-         cprd_lookup.code = additional_int.data_code
+      ON cprd_lookup.lookup_type = additional.lookup_type AND
+         cprd_lookup.code = additional.data_code
   LEFT JOIN cdm5.source_to_concept_map AS unit_map
-      ON unit_map.source_code = additional_int.unit_code AND
+      ON unit_map.source_code = additional.unit_code AND
          unit_map.source_vocabulary_id = 'CPRD_UNIT'
   WHERE clinical.eventdate IS NOT NULL AND target_concept.domain_id = 'Measurement'
 ;
