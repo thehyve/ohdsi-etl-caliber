f8ee0da86f54b09d432cf8b59744be821e9a52e6
Maxim Moinat <maxim@thehyve.nl>
2017-12-06 11:48:27 +0100

    add join to HES Specialty source concept

--- sql/loading/provider.sql
+++ sql/loading/provider.sql
@@ -42,11 +42,18 @@ INSERT INTO cdm5.provider (
                                           AS provider_id,
 
     NULL                                  AS gender_concept_id,
-    NULL                                  AS gender_source_value,
-    coalesce(target_concept_id, 38004514) AS specialty_concept_id,
+
+    coalesce(target_concept_id, 0)        AS specialty_concept_id,
+
+    provider_mapping.concept_id_2         AS specialty_source_concept_id,
+
     tretspef                              AS specialty_source_value
 
   FROM @source_schema.hes_op_clinical AS hoc
     LEFT JOIN cdm5.source_to_concept_map AS cms2_map
       ON hoc.tretspef = cms2_map.source_code
-         AND cms2_map.source_vocabulary_id = 'JNJ_CPRD_PROV_CMS2';
+         AND cms2_map.source_vocabulary_id = 'JNJ_CPRD_PROV_CMS2'
+    LEFT JOIN cdm5.concept AS source_concept
+      ON staff.role :: TEXT = source_concept.concept_code AND
+         source_concept.vocabulary_id = 'HES Specialty'
+;
